4036222b0a47f5f3b00baaacb568078e1d50e96d
Dennis Wong <dennis@Denniss-MacBook-Air.local>
2025-06-02 03:59:44 +0800

    test

--- model/chats.php
+++ model/chats.php
@@ -4,9 +4,9 @@
 
         static function _scopes() {
             return [
-                'is_merchant' => fn($merchant_id) => return self::where('merchant_id = %i', $merchant_id);
-                'is_pinned' => fn() => return self::where("pin > 0");
-                'recent' => fn($updated_at) => return self::where('updated_at > %s)', $updated_at);
+                'is_merchant' => fn($merchant_id) => self::where('merchant_id = %i', $merchant_id),
+                'is_pinned' => fn() => self::where("pin > 0"),
+                'recent' => fn($updated_at) => self::where('updated_at > %s)', $updated_at),
             ];
         }
 
