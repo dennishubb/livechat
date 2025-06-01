3222ca3dcd78d1a14849a927d39e94abd47a3174
Dennis Wong <dennis@Denniss-MacBook-Air.local>
2025-06-02 01:01:38 +0800

    test

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 
     require_once(__DIR__.'/include/init.php');
-    require_once(__DIR__.'/include/meekroDB_3.1.3.php');
-    require_once(__DIR__.'/include/meekroORM_3.1.3.php');
     require_once(__DIR__.'/include/common.php');
     require_once(__DIR__.'/include/routes.php');
 
     define('ROOT', __DIR__);
+
+    print_r($url);
 
 ?>
