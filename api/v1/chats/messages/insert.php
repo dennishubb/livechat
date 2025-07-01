ce0c2e9855ee70ca5f6498f44f61ed9bea6164b7
Dennis Wong <dennis@Denniss-MacBook-Air.local>
2025-07-01 18:48:18 +0800

    change

--- api/v1/chats/messages/insert.php
+++ api/v1/chats/messages/insert.php
@@ -4,6 +4,24 @@
 
     parse_request(['merchant_id', 'user_id', 'admin_id', 'message', 'source', 'status', 'created_datetime']);
 
+    include(ROOT.'/model/chat.php');
+
+    //create or update chatroom whenever a message is sent
+    $chat = Chat::Search("SELECT id FROM chats WHERE user_id = $i", $user_id);
+    if(!isset($chat->id)){
+        $chat = new Chat();
+        $chat->merchant_id = $merchant_id;
+        $chat->user_id = $user_id;
+        $chat->last_message = $message;
+        $chat->last_message_user_id = $user_id;
+        $chat->save();
+        $chat_id = DB::insertId();
+    }else{
+        $chat->last_message = $message;
+        $chat->last_message_user_id = $admin_id ? $admin_id : $user_id;
+        $chat->save();
+    }
+    
     $message = new Message();
 
     $message->merchant_id   = $merchant_id;
@@ -13,6 +31,7 @@
     $message->status        = $status;
     $message->created_at    = $created_datetime;
     $message->updated_at    = $created_datetime;
+    if(!empty($chat_id)) $message->chat_id = $chat_id;
     $message->save();
     $res['id'] = DB::insertId();
 
--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-
-	$url = 'http://www.test.com/api/v1/chat/deleteMessage?a=b&c=d&e=f';
-
-	print_r(parse_url($url));
-
-?>
