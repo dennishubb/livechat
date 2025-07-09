12b30125e96b1f209075ef48625448f02a9f9137
Dennis Wong <dennis@Denniss-MacBook-Air.local>
2025-07-09 11:22:23 +0800

    change

--- api/v1/chats/messages/update.php
+++ api/v1/chats/messages/update.php
@@ -8,7 +8,6 @@
 
     if(!$message->has('id')) http_response(code:404);
 
-    if(!isset($message->id)) http_response(code:404);
     if($message->merchant_id != $merchant_id) http_response(code:403);
     if(!in_array($status, ['SKIPPED','SENT'])) http_response(code:400);
 
