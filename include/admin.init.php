726e292cf45249a5ca5aea91a8b04635852f2c05
Dennis Wong <dennis@Denniss-MacBook-Air.local>
2025-07-16 05:15:03 +0800

    public

--- include/admin.init.php
+++ include/admin.init.php
@@ -2,4 +2,9 @@
 
     define('ADMIN_VERSION', 'update whenever deploy');
 
+    $html = file_get_contents(ROOT.'/admin/init.html');
+    $html = str_replace('{Version}', 'testing', $html);
+    $html = str_replace('{Root}', $ROOT, $html);
+
+    exit($html);
 ?>
