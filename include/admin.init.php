84ec9de5ed6b08bd89080979ba82be00c0d0c70a
Dennis Wong <dennis@Denniss-MacBook-Air.local>
2025-07-16 05:45:48 +0800

    public

--- include/admin.init.php
+++ include/admin.init.php
@@ -4,7 +4,6 @@
 
     $html = file_get_contents(ROOT.'/admin/init.html');
     $html = str_replace('{Version}', ADMIN_VERSION, $html);
-    $html = str_replace('{Root}', ROOT, $html);
 
     exit($html);
 ?>
