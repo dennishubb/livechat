de29bcc0ef578c71e8c277fc17e7f9ef3e3400e4
Dennis Wong <dennis@Denniss-MacBook-Air.local>
2025-07-17 01:33:20 +0800

    public

--- include/admin.init.php
+++ include/admin.init.php
@@ -6,4 +6,8 @@
     // $html = str_replace('{Version}', ADMIN_VERSION, $html);
 
     // exit($html);
+
+    if(!file_exists(ROOT.$url['path'].'.html')) exit('file not found');
+    $NOW = date('Y-m-d H:i:s');
+    include(ROOT.$url['path'].'.html');
 ?>
