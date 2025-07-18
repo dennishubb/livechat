48c3f03b1a82bbde9cfba673ee044f0204f995a5
Dennis Wong <dennis@Denniss-MacBook-Air.local>
2025-07-19 06:38:11 +0800

    public

--- backoffice/views/main/main.js
+++ backoffice/views/main/main.js
@@ -3,7 +3,7 @@ $(function() {
 	console.log('main');
 	//initialize styles and html
 	const _ = require('underscore');
-	$('head').append('<link rel="stylesheet" type="text/css" href="./main.css">');
-	$('head').append('<link rel="stylesheet" type="text/css" href="./cssA.css">');
+	$('head').append('<link rel="stylesheet" type="text/css" href="/views/main/main.css">');
+	$('head').append('<link rel="stylesheet" type="text/css" href="/views/main/cssA.css">');
 
 });
