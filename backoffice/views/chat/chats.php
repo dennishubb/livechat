fe7b691b806d690939283d246075569f48bf43ae
Dennis Wong <dennis@Denniss-MacBook-Air.local>
2025-07-18 15:21:05 +0800

    public

--- backoffice/views/chat/chats.php
+++ backoffice/views/chat/chats.php
@@ -1,8 +1,8 @@
 <?php
 
-    $main_html = str_replace('<!--CUSTOM_CSS-->', '<link rel="stylesheet" type="text/css" href="./chats.css">', $main_html);
-    $main_html = str_replace('<!--CUSTOM_JS-->', '<script type="text/javascript" src="./chats.js"></script>', $main_html);
+    $main_html = str_replace('<!--CUSTOM_CSS-->', '<link rel="stylesheet" type="text/css" href="'.ROOT.'/views/chat/chats.css">', $main_html);
+    $main_html = str_replace('<!--CUSTOM_JS-->', '<script type="text/javascript" src="'.ROOT.'/views/chat/chats.js"></script>', $main_html);
 
-    echo $main_html;
+    exit($main_html);
 
 ?>
