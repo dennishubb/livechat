a04c224cadaa36a1c47c4f34710138da5a1d567b
Dennis Wong <dennis@Denniss-MacBook-Air.local>
2025-07-19 07:52:40 +0800

    public

--- backoffice/views/chatlist/chatlist.js
+++ backoffice/views/chatlist/chatlist.js
@@ -1,8 +1,3 @@
 $(function() {
-
-	console.log('chatlist');
-	//initialize styles and html
 	$('head').append('<link rel="stylesheet" type="text/css" href="/views/chatlist/chatlist.css">');
-	$("#content").load('/views/chatlist/chatlist.html');
-
 });
---
+++ backoffice/views/errors/404.js
@@ -0,0 +1,6 @@
+$(function() {
+
+	console.log('404');
+	$('head').append('<link rel="stylesheet" type="text/css" href="/views/errors/404.css">');
+
+});
