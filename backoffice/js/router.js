9c1f63c807096171c901679f4de6edc2a8103a17
Dennis Wong <dennis@Denniss-MacBook-Air.local>
2025-07-19 07:47:46 +0800

    public

--- backoffice/js/router.js
+++ backoffice/js/router.js
@@ -2,15 +2,15 @@ const route_path = function() {
 	var path = location.pathname.split("/");
 
 	if(typeof path[1] === 'undefined') {
-		path = '/views/errors/404.html';
+		path = '../views/errors/404.html';
 	}
 	else {
 		switch(path[1]){
 			case 'chat':
-				path = '/views/chatlist/chatlist.html';
+				path = '../views/chatlist/chatlist.html';
 				break;
 			default:
-				path = '/views/errors/404.html';
+				path = '../views/errors/404.html';
 				break;
 		}
 	}
