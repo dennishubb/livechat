e83ed77a8e7accb0cb5a67b655e4fefe9f7a3b3e
Dennis Wong <dennis@Denniss-MacBook-Air.local>
2025-07-19 07:32:50 +0800

    public

--- backoffice/index.php
+++ backoffice/index.php
@@ -4,7 +4,9 @@
 
     //check for session, if exist and active go to page, else login
     //when login, return token << token has merchant_id
+
+    exit(file_get_contents(ROOT.'/views/init.html'));
     
-    include(ROOT.'/include/route.php');
+    // include(ROOT.'/include/route.php');
 
 ?>
--- backoffice/js/router.js
+++ backoffice/js/router.js
@@ -1,5 +1,7 @@
+console.log('router');
+
 function route_path() {
-	
+	console.log('route path');
 	var path = location.pathname.split("/");
 
 	return path;
