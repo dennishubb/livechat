17051969f4a1d6d47afebbc68bab22e021d1f059
Dennis Wong <dennis@Denniss-MacBook-Air.local>
2025-07-18 20:26:36 +0800

    public

--- backoffice/views/chat/chats.js
+++ backoffice/views/chat/chats.js
@@ -1,8 +1,8 @@
 $(function() {
 	// Code to be executed once the DOM is ready
-	console.log("chat");
-	// $.get('./chats.html', function(html){
-	// 	console.log('chatlist?');
-	// 	$("#content").html(html);
-	// });
+	
+	$.get('./chats.html', function(html){
+		console.log('chatlist?');
+		$("#content").html(html);
+	});
 });
