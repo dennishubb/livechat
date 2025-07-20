dc311215568e631fc0b2bee07f39c976c2fab4ac
Dennis Wong <dennis@Denniss-MacBook-Air.local>
2025-07-21 03:54:01 +0800

    add

--- api/index.php
+++ api/index.php
@@ -3,14 +3,6 @@
     header('Access-Control-Allow-Origin: http://backoffice.livechat.com');
     header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
     header('Access-Control-Allow-Headers: Origin, Content-Type, x-authorization-key');
-
-    if($_SERVER['REQUEST_METHOD'] === 'OPTIONS'){
-        exit();
-    }
-
-    $token = 'QrtYujorR1Y4gGaxO2CAilCGQzTiLL6tM84Pap5y14vY57v3W5IMKthcCGPEEVmV';
-    $req_token = trim(getallheaders()['x-authorization-key']);
-    if($req_token !== $token) http_response(code:403);
     
     define('ROOT', __DIR__);
 
@@ -19,6 +11,10 @@
     require_once(ROOT.'/include/routes.php');
 
     DB::insert('debug', ['text' => json_encode(getallheaders())]);
+
+    $token = 'QrtYujorR1Y4gGaxO2CAilCGQzTiLL6tM84Pap5y14vY57v3W5IMKthcCGPEEVmV';
+    $req_token = trim(getallheaders()['x-authorization-key']);
+    if($req_token !== $token) http_response(code:403);
 
     if(!file_exists(ROOT.$url['path'].'.php')) exit('file not found');
     $NOW = date('Y-m-d H:i:s');
