f0686c648cd9f7004ff143b966043a0024612027
Dennis Wong <dennis@Denniss-MacBook-Air.local>
2025-07-21 03:34:15 +0800

    add

--- backoffice/js/app.js
+++ backoffice/js/app.js
@@ -37,6 +37,7 @@ require(['jquery', 'router'], function($, router) {
 		$.ajaxPrefilter(function( options, originalOptions, jqXHR ) {
 			console.log("PREFILTER");
 			jqXHR.setRequestHeader('X-Authorization-Key', 'QrtYujorR1Y4gGaxO2CAilCGQzTiLL6tM84Pap5y14vY57v3W5IMKthcCGPEEVmV');
+			console.log(jqXHR);
 		});
 	});
 });
