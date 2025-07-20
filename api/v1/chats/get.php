27764112b828eeda189d2abf6f9c1902c6753f43
Dennis Wong <dennis@Denniss-MacBook-Air.local>
2025-07-21 06:23:31 +0800

    add

--- api/v1/chats/get.php
+++ api/v1/chats/get.php
@@ -14,7 +14,7 @@
 	$updated_at = date('Y-m-d H:i:s', strtotime("-1 day"));
 
 	if(is_array($merchant_id)){
-		$chats = DB::query("SELECT merchant_id, user_id, pinned, pin_id, last_message, last_message_user_id, created_at, updated_at FROM chats WHERE merchant_id IN %li AND updated_at >= %s ORDER BY updated_at DESC $queryLimit", $merchant_id, $updated_at);
+		$chats = DB::query("SELECT merchant_id, user_id, user_name, pinned, pin_id, last_message, last_message_user_id, last_message_user_name, created_at, updated_at FROM chats WHERE merchant_id IN %li AND updated_at >= %s ORDER BY updated_at DESC $queryLimit", $merchant_id, $updated_at);
 	}else{
 		$chats = DB::query("SELECT merchant_id, user_id, pinned, pin_id, last_message, last_message_user_id, created_at, updated_at FROM chats WHERE merchant_id = %i AND updated_at >= %s ORDER BY updated_at DESC $queryLimit", $merchant_id, $updated_at);
 	}
