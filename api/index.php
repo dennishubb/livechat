ca63b85efbfe69dc0337effa6f2871aded8faf89
Dennis Wong <dennis@Denniss-MacBook-Air.local>
2025-07-21 03:52:55 +0800

    add

--- api/index.php
+++ api/index.php
@@ -4,12 +4,14 @@
     header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
     header('Access-Control-Allow-Headers: Origin, Content-Type, x-authorization-key');
 
-    if($_SERVER['REQUEST_METHOD'] !== 'OPTIONS'){
-        $token = 'QrtYujorR1Y4gGaxO2CAilCGQzTiLL6tM84Pap5y14vY57v3W5IMKthcCGPEEVmV';
-        $req_token = trim(getallheaders()['x-authorization-key']);
-        if($req_token !== $token) http_response(code:403);
+    if($_SERVER['REQUEST_METHOD'] === 'OPTIONS'){
+        exit();
     }
 
+    $token = 'QrtYujorR1Y4gGaxO2CAilCGQzTiLL6tM84Pap5y14vY57v3W5IMKthcCGPEEVmV';
+    $req_token = trim(getallheaders()['x-authorization-key']);
+    if($req_token !== $token) http_response(code:403);
+    
     define('ROOT', __DIR__);
 
     require_once(ROOT.'/include/db.php');
