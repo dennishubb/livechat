1c086d532b41cc0e5fa7a8b96c33379e40ee455e
Dennis Wong <dennis@Denniss-MacBook-Air.local>
2025-07-21 03:58:19 +0800

    add

--- api/index.php
+++ api/index.php
@@ -11,10 +11,9 @@
     require_once(ROOT.'/include/routes.php');
 
     DB::insert('debug', ['text' => json_encode(getallheaders())]);
-
+    DB::insert('debug', ['text' => json_encode($_SERVER)]);
     $token = 'QrtYujorR1Y4gGaxO2CAilCGQzTiLL6tM84Pap5y14vY57v3W5IMKthcCGPEEVmV';
-    $headers = getallheaders();
-    $req_token = trim(isset($headers['x-authorization-key']) ? $headers['x-authorization-key']:$headers['X-Authorization-Key']);
+    $req_token = trim(getallheaders()['x-authorization-key']);
     if($req_token !== $token) http_response(code:403);
 
     if(!file_exists(ROOT.$url['path'].'.php')) exit('file not found');
