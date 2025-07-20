43b77af9d123aed4ee2311016a2daf8e70a3d2a8
Dennis Wong <dennis@Denniss-MacBook-Air.local>
2025-07-21 03:06:44 +0800

    add

--- api/index.php
+++ api/index.php
@@ -1,11 +1,13 @@
 <?php
 
+    header('Access-Control-Allow-Origin: backoffice.livechat.com');
+
     $token = 'QrtYujorR1Y4gGaxO2CAilCGQzTiLL6tM84Pap5y14vY57v3W5IMKthcCGPEEVmV';
     $req_token = trim(getallheaders()['X-Authorization-Key']);
     if($req_token !== $token) http_response(code:403);
 
     define('ROOT', __DIR__);
-    
+
     require_once(ROOT.'/include/db.php');
     require_once(ROOT.'/include/common.php');
     require_once(ROOT.'/include/routes.php');
