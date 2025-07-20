f867d86a69b7d2a563e4f58c47a3a0a76d6e51ad
Dennis Wong <dennis@Denniss-MacBook-Air.local>
2025-07-21 03:04:24 +0800

    add

--- backoffice/views/chatlist/chatlist.js
+++ backoffice/views/chatlist/chatlist.js
@@ -2,9 +2,8 @@ $(function() {
 	$('head').append('<link rel="stylesheet" type="text/css" href="/views/chatlist/chatlist.css">');
 
 	var request = {'merchant_id':5,};
-	$.post('api.livechat.com/chats/get', request, function(data){
+	$.post('http://api.livechat.com/chats/get', request, function(data){
 		console.log("api");
-		console.log()
 		console.log(data);
 	});
 });
