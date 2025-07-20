be94aff904b34f3fa03a7fd585639cb2af20b6fa
Dennis Wong <dennis@Denniss-MacBook-Air.local>
2025-07-21 03:03:53 +0800

    add

--- backoffice/views/chatlist/chatlist.js
+++ backoffice/views/chatlist/chatlist.js
@@ -1,8 +1,10 @@
 $(function() {
 	$('head').append('<link rel="stylesheet" type="text/css" href="/views/chatlist/chatlist.css">');
 
-	var request = {'merchant_id':5, }
+	var request = {'merchant_id':5,};
 	$.post('api.livechat.com/chats/get', request, function(data){
+		console.log("api");
+		console.log()
 		console.log(data);
 	});
 });
--- index.php
+++ index.php
@@ -5,10 +5,10 @@
 
     define('ROOT', __DIR__);
 
-    if($path[1] === 'api') require_once(__DIR__.'/include/api.init.php');
-    else if($path[1] === 'admin') require_once(__DIR__.'/include/admin.init.php');
-    else if($path[1] === 'public') require_once(__DIR__.'/include/public.init.php');
+    // if($path[1] === 'api') require_once(__DIR__.'/include/api.init.php');
+    // else if($path[1] === 'admin') require_once(__DIR__.'/include/admin.init.php');
+    // else if($path[1] === 'public') require_once(__DIR__.'/include/public.init.php');
 
-    http_response(code:500);
+    // http_response(code:500);
     
 ?>
