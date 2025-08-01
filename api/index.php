5e325a6f4ecb4d9a3e2683b391d6b6a38d0111a2
Dennis Wong <dennis@Denniss-MacBook-Air.local>
2025-08-01 18:44:57 +0800

    add public

--- api/index.php
+++ api/index.php
@@ -1,6 +1,6 @@
 <?php
 
-    header('Access-Control-Allow-Origin: http://backoffice.livechat.com, http://public.livechat.com');
+    header('Access-Control-Allow-Origin: http://backoffice.livechat.com http://public.livechat.com');
     header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
     header('Access-Control-Allow-Headers: Origin, Content-Type, X-Authorization-Key');
     
